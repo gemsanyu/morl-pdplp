0e461ed19d1a8b971492ff9dc5992b9594b80a3e
gemsanyu <git.gemilang.santiyuda@gmail.com>
2023-02-28 10:34:57 +0800

    fix attention compatation in decode

--- model/agent.py
+++ model/agent.py
@@ -41,7 +41,7 @@ class Agent(torch.nn.Module):
         self.project_embeddings = Linear(embed_dim, 3*embed_dim, bias=False)
         self.project_fixed_context = Linear(embed_dim, embed_dim, bias=False)
         current_state_dim = embed_dim + num_vehicle_dynamic_features
-        self.project_current_state = Linear(current_state_dim, embed_dim, bias=False)
+        self.project_current_vehicle_state = Linear(current_state_dim, embed_dim, bias=False)
         self.project_node_state = Linear(num_node_dynamic_features, 3*embed_dim, bias=False)
         self.project_out = Linear(embed_dim, embed_dim, bias=False)
         self.to(self.device)
@@ -68,6 +68,8 @@ class Agent(torch.nn.Module):
         num_vehicles_cum = np.insert(np.cumsum(num_vehicles),0,0)
         total_num_vehicles = np.sum(num_vehicles)
         feasibility_mask = torch.cat(feasibility_mask)
+        
+        print("_---------")
         # prepare the static to be repeated as many as the number of vehicles
         # in each batch size
         glimpse_V_static_list = [glimpse_V_static[:, i].unsqueeze(1).expand((-1,num_vehicles[i],-1,-1)) for i in range(batch_size)]
@@ -77,12 +79,12 @@ class Agent(torch.nn.Module):
         logit_K_static_list = [logit_K_static[i,:].unsqueeze(0).expand((num_vehicles[i],-1,-1)) for i in range(batch_size)]
         logit_K_static = torch.cat(logit_K_static_list, dim=0)
         # now decode
-        fixed_context = fixed_context.unsqueeze(1)  
-        fixed_context_each_vehicle = [torch.cat([fixed_context[i].expand((num_vehicles[i],-1)),vehicle_dynamic_features[i]], dim=1) for i in range(batch_size)]
-        fixed_context_each_vehicle = torch.cat([fixed_context_each_vehicle[i] for i in range(batch_size)], dim=0)
+        current_vehicle_state_list = [torch.cat([prev_node_embeddings[i], vehicle_dynamic_features[i]], dim=1) for i in range(batch_size)]
+        current_vehicle_state = torch.cat(current_vehicle_state_list, dim=0)
+        # repeat fixed context for each vehicle
         fixed_context = torch.cat([fixed_context[i].unsqueeze(0).expand((num_vehicles[i],-1,-1)) for i in range(batch_size)])
         if param_dict is None:
-            projected_current_state = self.project_current_state(fixed_context_each_vehicle).unsqueeze(1) 
+            projected_current_vehicle_state = self.project_current_vehicle_state(current_vehicle_state).unsqueeze(1) 
             node_dynamic_embeddings = self.project_node_state(torch.cat([node_dynamic_features[i] for i in range(batch_size)], dim=0))
             glimpse_V_dynamic, glimpse_K_dynamic, logit_K_dynamic = node_dynamic_embeddings.chunk(3, dim=-1)
         glimpse_V_dynamic = self._make_heads(glimpse_V_dynamic)
@@ -90,14 +92,25 @@ class Agent(torch.nn.Module):
         glimpse_V = glimpse_V_static + glimpse_V_dynamic
         glimpse_K = glimpse_K_static + glimpse_K_dynamic
         logit_K = logit_K_static + logit_K_dynamic
-        query = fixed_context + projected_current_state
+        query = fixed_context + projected_current_vehicle_state
         glimpse_Q = query.view(total_num_vehicles, self.n_heads, 1, self.key_size)
         glimpse_Q = glimpse_Q.permute(1,0,2,3)
         compatibility = glimpse_Q@glimpse_K.permute(0,1,3,2) / math.sqrt(glimpse_Q.size(-1)) # glimpse_K => n_heads, batch_size, num_items, embed_dim
         compatibility = compatibility + feasibility_mask.unsqueeze(0).unsqueeze(2).float().log()
-        attention = torch.softmax(compatibility, dim=-1)
-        heads = attention@glimpse_V
+        # ini compatibility per vehicle untuk semua nodes
+        # tapi attention sebenarnya harus per batch
+        # jadi harus diflatten per batch, agar semua vec x customer jadi satu untuk disoftmax
+        # lalu nanti di-reshape ulang
 
+        compatibility = [compatibility[:, num_vehicles_cum[i-1]:num_vehicles_cum[i]] for i in range(1,batch_size+1)]
+        attention = [compatibility[i].reshape(self.n_heads,1,1,num_vehicles[i]*num_nodes) for i in range(batch_size)]
+        attention = [torch.softmax(attention[i], dim=-1) for i in range(batch_size)]
+        attention = torch.cat([attention[i].reshape(self.n_heads,num_vehicles[i],1,num_nodes) for i in range(batch_size)], dim=1)
+        heads = attention@glimpse_V
+        # harus check bener gak yang kayak gini
+        # kita concat vehicles across batch
+        # sama kita isolasi per batch, tapi vehicle x nodees diflatten
+        # kalau sama harusnya bener
         concated_heads = heads.permute(1,2,0,3).contiguous()
         concated_heads = concated_heads.view(total_num_vehicles, 1, self.embed_dim)
         if param_dict is None:
@@ -108,13 +121,13 @@ class Agent(torch.nn.Module):
         # now we need to re-split the logits into their respective batch
         logits = [logits[num_vehicles_cum[i-1]:num_vehicles_cum[i]].flatten() for i in range(1,batch_size+1)]
         probs = [torch.softmax(logits[i],dim=-1) for i in range(batch_size)]
-        
         select_result_list = [self.select(probs[i]) for i in range(batch_size)]
         action_list = [select_result_list[i][0] for i in range(batch_size)]
         logprob_list = [select_result_list[i][1] for i in range(batch_size)]
         entropy_list = [select_result_list[i][2] for i in range(batch_size)]
         selected_vec = [int(action_list[i]/num_nodes) for i in range(batch_size)]
         selected_node = [int(action_list[i]%num_nodes) for i in range(batch_size)] 
+        print("---------_")
         return selected_vec, selected_node, logprob_list, entropy_list
 
     def select(self, probs):
