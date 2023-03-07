03b28af1771a08d257f3627bc911487f9ad55fea
gemsanyu <git.gemilang.santiyuda@gmail.com>
2023-03-07 18:13:02 +0800

    as fast as expected, vectorized the env

--- bpdplp/bpdplp_env.py
+++ bpdplp/bpdplp_env.py
@@ -45,7 +45,11 @@ def compute_travel_time(distance, current_time, time_horizon, speed_profile):
     travel_time = arrived_time-current_time
     return travel_time    
 
-
+"""
+    we need to add dummy vehicles,
+    so that all computation here is vectorized
+    this is really bottleneck
+"""
 class BPDPLP_Env(object):
     def __init__(self, 
                  num_vehicles, 
@@ -65,9 +69,11 @@ class BPDPLP_Env(object):
         
         self.num_vehicles = num_vehicles.numpy()
         self.num_vehicles_cum = np.insert(np.cumsum(self.num_vehicles),0,0)
+        self.max_num_vehicles = int(np.max(self.num_vehicles))
         self.total_num_vehicles = int(self.num_vehicles_cum[-1])
         self.max_capacity = max_capacity.numpy()
         self.batch_size, self.num_nodes, _ = coords.shape
+        self.batch_vec_idx = np.repeat(np.arange(self.batch_size), self.max_num_vehicles)
         self.num_requests = (self.num_nodes-1)//2
         self.coords = coords.numpy()
         self.norm_coords = norm_coords.numpy()
@@ -81,20 +87,31 @@ class BPDPLP_Env(object):
         self.distance_matrix = distance_matrix.numpy()
         self.norm_distance_matrix = norm_distance_matrix.numpy()
         self.road_types = road_types.numpy()
+        
+        #for travel time computation
+        planning_time_repeated = self.planning_time[:,np.newaxis,np.newaxis]
+        planning_time_repeated = np.repeat(planning_time_repeated, self.max_num_vehicles, 1)
+        planning_time_repeated = np.repeat(planning_time_repeated, self.num_nodes, 2)
+        planning_time_repeated = planning_time_repeated.flatten()
+        self.time_horizons_repeated = TIME_HORIZONS*planning_time_repeated[:, np.newaxis]
         self.reset()
 
     def reset(self):
-        self.current_load = [np.asanyarray([0 for j in range(self.num_vehicles[i])], dtype=np.float32) for i in range(self.batch_size)]
-        self.current_time = [np.asanyarray([0 for j in range(self.num_vehicles[i])], dtype=np.float32) for i in range(self.batch_size)]
-        self.current_location_idx = [np.asanyarray([0 for j in range(self.num_vehicles[i])], dtype=int) for i in range(self.batch_size)]
+        self.current_load = np.zeros((self.batch_size, self.max_num_vehicles), dtype=np.float32)
+        self.current_time = np.zeros((self.batch_size, self.max_num_vehicles), dtype=np.float32)
+        self.current_location_idx = np.zeros((self.batch_size, self.max_num_vehicles), dtype=int)
+        self.is_not_dummy_mask = np.ones((self.batch_size, self.max_num_vehicles, self.num_nodes), dtype=bool)
+        for i in range(self.batch_size):
+            self.is_not_dummy_mask[i,self.num_vehicles[i]:,:] = False
         self.is_node_visited = np.zeros((self.batch_size, self.num_nodes), dtype=bool)
         self.is_node_visited[:, 0] = True
         self.request_assignment = np.full((self.batch_size, self.num_requests), -1, dtype=int)
-        self.travel_cost = [[0 for j in range(self.num_vehicles[i])] for i in range(self.batch_size)]
-        self.late_penalty = [[0 for j in range(self.num_vehicles[i])] for i in range(self.batch_size)]
-        self.arrived_time = [[[] for j in range(self.num_vehicles[i])] for i in range(self.batch_size)]
-        self.tour_list = [[[] for j in range(self.num_vehicles[i])] for i in range(self.batch_size)]
-        self.departure_time_list = [[[] for j in range(self.num_vehicles[i])] for i in range(self.batch_size)]
+        self.travel_cost = np.zeros((self.batch_size, self.max_num_vehicles), dtype=np.float32)
+        self.late_penalty = np.zeros((self.batch_size, self.max_num_vehicles), dtype=np.float32)
+        self.num_visited_nodes = np.zeros((self.batch_size, self.max_num_vehicles), dtype=int)
+        self.arrived_time = np.zeros((self.batch_size, self.max_num_vehicles, self.num_nodes), dtype=np.float32)
+        self.tour_list = np.zeros((self.batch_size, self.max_num_vehicles, self.num_nodes), dtype=int)
+        self.departure_time_list = np.zeros((self.batch_size, self.max_num_vehicles, self.num_nodes), dtype=np.float32)
         self.travel_time_list = self.get_travel_time()
         
 
@@ -119,30 +136,27 @@ class BPDPLP_Env(object):
     """
     @property
     def vehicle_dynamic_features(self):
-        norm_current_coords = [self.norm_coords[i, self.current_location_idx[i]] for i in range(self.batch_size)]
-        norm_current_load = [self.current_load[i][:, np.newaxis]/self.max_capacity[i] for i in range(self.batch_size)]
-        norm_current_time = [self.current_time[i][:, np.newaxis]/self.planning_time[i] for i in range(self.batch_size)]
-        features = [np.concatenate([norm_current_coords[i], norm_current_load[i], norm_current_time[i]], axis=1) for i in range(self.batch_size)]
+        norm_current_coords = self.norm_coords[self.batch_vec_idx, self.current_location_idx.flatten(),:]
+        norm_current_coords = norm_current_coords.reshape(self.batch_size, self.max_num_vehicles, 2)
+        norm_current_load = (self.current_load/self.max_capacity[:, np.newaxis])[:,:, np.newaxis]
+        norm_current_time = (self.current_time/self.planning_time[:, np.newaxis])[:,:, np.newaxis]
+        features = np.concatenate([norm_current_coords,norm_current_load,norm_current_time], axis=-1)
         return features
     
 
     def get_travel_time(self):
-        distances_list = [self.distance_matrix[i, self.current_location_idx[i], :] for i in range(self.batch_size)]
-        distances_list = np.concatenate(distances_list).flatten()
-        # print(distances_list, distances_list.shape, "BRAH")
-        # print(self.current_location_idx, "BROH")
-        current_time_list = [np.repeat(self.current_time[i][:, np.newaxis], self.num_nodes, axis=1) for i in range(self.batch_size)]
-        current_time_list = np.concatenate(current_time_list).flatten()
-        planning_time_list = [np.asanyarray([self.planning_time[i]]*self.num_vehicles[i]) for i in range(self.batch_size)]
-        time_horizon_list = [planning_time_list[i][:,np.newaxis]*TIME_HORIZONS for i in range(self.batch_size)]
-        time_horizon_list = [np.repeat(time_horizon_list[i][:,np.newaxis,:], self.num_nodes, axis=1) for i in range(self.batch_size)]
-        time_horizon_list = np.concatenate(time_horizon_list).reshape(-1, 6)
-        road_types_list = [self.road_types[i, self.current_location_idx[i], :] for i in range(self.batch_size)]
-        speed_profile_list = [SPEED_PROFILES[road_types_list[i], :] for i in range(self.batch_size)]
-        speed_profile_list = np.concatenate(speed_profile_list).reshape(-1, 5)
+        current_location_idx = self.current_location_idx.flatten()
+        distances_list = self.distance_matrix[self.batch_vec_idx, current_location_idx,:].flatten()
+        current_time_list = self.current_time[:,:,np.newaxis]
+        current_time_list = np.repeat(current_time_list,self.num_nodes,axis=2).flatten()
+        time_horizon_list = self.time_horizons_repeated
+        road_types_list = self.road_types[self.batch_vec_idx, current_location_idx,:].flatten()
+        speed_profile_list = SPEED_PROFILES[road_types_list,:]
         travel_time_list = compute_travel_time_vectorized(distances_list, current_time_list, time_horizon_list, speed_profile_list)
-        travel_time_list = [travel_time_list[self.num_vehicles_cum[i-1]*self.num_nodes:self.num_vehicles_cum[i]*self.num_nodes] for i in range(1,self.batch_size+1)]
-        travel_time_list = [travel_time_list[i].reshape(self.num_vehicles[i], -1) for i in range(self.batch_size)]
+        travel_time_list = travel_time_list.reshape((self.batch_size, self.max_num_vehicles, self.num_nodes))
+        # travel_time_list = [travel_time_list[self.num_vehicles_cum[i-1]*self.num_nodes:self.num_vehicles_cum[i]*self.num_nodes] for i in range(1,self.batch_size+1)]
+        # travel_time_list = [travel_time_list[i].reshape(self.num_vehicles[i], -1) for i in range(self.batch_size)]
+        
         # print("-----------------------------")
         # distances_listv2 = [self.distance_matrix[i, self.current_location_idx[i], :] for i in range(self.batch_size)]
         # current_time_listv2 = [self.current_time[i] for i in range(self.batch_size)]
@@ -184,8 +198,8 @@ class BPDPLP_Env(object):
     @property
     def node_dynamic_features(self):
         travel_time_list = self.travel_time_list
-        norm_travel_time_list = [travel_time_list[i]/self.planning_time[i] for i in range(self.batch_size)]
-        norm_travel_time_list = [norm_travel_time_list[i][:,:,np.newaxis] for i in range(self.batch_size)]
+        norm_travel_time_list = travel_time_list/self.planning_time[:,np.newaxis,np.newaxis]
+        norm_travel_time_list = norm_travel_time_list[:,:,:,np.newaxis]
         return norm_travel_time_list
 
     @property
@@ -193,22 +207,22 @@ class BPDPLP_Env(object):
         is_pickup_visited = self.is_node_visited[:,1:self.num_requests+1]
         is_delivery_visited = self.is_node_visited[:,self.num_requests+1:]
         # for pickup, feasible if taken do not make load exceed capacity and not visited yet
-        pickup_demands = self.demands[:,1:self.num_requests+1]
-        current_load_if_pickup = [self.current_load[i][:, np.newaxis] + pickup_demands[i,:] for i in range(self.batch_size) ]
-        is_pickup_exceed_load = [current_load_if_pickup[i]>self.max_capacity[i] for i in range(self.batch_size)]
-        
-        is_pickup_feasible = [np.logical_and(np.logical_not(is_pickup_visited[np.newaxis,i,:]), np.logical_not(is_pickup_exceed_load[i])) for i in range(self.batch_size)]
-        
+        pickup_demands = self.demands[:,np.newaxis,1:self.num_requests+1]
+        current_load_if_pickup = self.current_load[:,:,np.newaxis] + pickup_demands
+        is_pickup_exceed_load = current_load_if_pickup > self.max_capacity[:,np.newaxis,np.newaxis]
+        is_pickup_feasible = np.logical_and(np.logical_not(is_pickup_visited[:,np.newaxis,:]), np.logical_not(is_pickup_exceed_load))
         # for delivery, feasible for the k-th vehicle 
         # if pickup is visited, and is assigned to the k-th vehicle
         # and it is not visited yet
-        is_assigned_to_vec = [self.request_assignment[i][np.newaxis, :] == np.arange(self.num_vehicles[i])[:, np.newaxis] for i in range(self.batch_size)]
-        is_delivery_feasible = [np.logical_and(is_pickup_visited[np.newaxis, i], np.logical_not(is_delivery_visited[np.newaxis, i])) for i in range(self.batch_size)]
-        
-        is_delivery_feasible = [np.repeat(is_delivery_feasible[i],self.num_vehicles[i], axis=0) for i in range(self.batch_size)]
-        is_delivery_feasible = [np.logical_and(is_delivery_feasible[i], is_assigned_to_vec[i]) for i in range(self.batch_size)]
-        
-        mask = [np.concatenate([np.asanyarray([[False]]*self.num_vehicles[i]), is_pickup_feasible[i], is_delivery_feasible[i]], axis=1) for i in range(self.batch_size)]
+        # is_assigned_to_vec = 
+        vehicle_idx = np.arange(self.max_num_vehicles)[np.newaxis,:,np.newaxis]
+        is_assigned_to_vec = self.request_assignment[:,np.newaxis,:] == vehicle_idx
+        is_delivery_feasible = np.logical_and(is_assigned_to_vec, is_pickup_visited[:,np.newaxis,:])
+        is_delivery_feasible = np.logical_and(is_delivery_feasible, np.logical_not(is_delivery_visited[:,np.newaxis,:]))
+        is_depot_feasible = np.asanyarray([[[False]]*self.max_num_vehicles]*self.batch_size)
+        mask = np.concatenate([is_depot_feasible, is_pickup_feasible, is_delivery_feasible], axis=2)
+        # lastly mask the dummy vehicles
+        mask = np.logical_and(mask, self.is_not_dummy_mask)
         return mask
     """
         i think we need to vectorize this, because a lot of it can be...
@@ -234,27 +248,38 @@ class BPDPLP_Env(object):
     """
     def service_node_by_vec(self, batch_idx, selected_vecs, selected_nodes):
         travel_time_list = self.travel_time_list
-        travel_time_vecs = [travel_time_list[i][selected_vecs[i], selected_nodes[i]] for i in batch_idx]
+        travel_time_vecs = travel_time_list[batch_idx, selected_vecs, selected_nodes]
         self.is_node_visited[batch_idx, selected_nodes] = True
         # isnp -> is_selected_node_pickup
         # assign the request to the vehicles
         isnp = selected_nodes <= self.num_requests
         self.request_assignment[batch_idx[isnp], selected_nodes[isnp]-1] = selected_vecs[isnp]
-        for i in batch_idx:
-            self.current_load[i][selected_vecs[i]] += self.demands[i, selected_nodes[i]]
-            self.tour_list[i][selected_vecs[i]] += [selected_nodes[i]]
-            self.departure_time_list[i][selected_vecs[i]] += [self.current_time[i][selected_vecs[i]]]
-            self.current_time[i][selected_vecs[i]] += travel_time_vecs[i]
-            if self.current_time[i][selected_vecs[i]] <= self.time_windows[i,selected_nodes[i],0]:
-                self.current_time[i][selected_vecs[i]] = self.time_windows[i,selected_nodes[i],0]
-            elif self.current_time[i][selected_vecs[i]] >self.time_windows[i,selected_nodes[i],1]:
-                self.late_penalty[i][selected_vecs[i]]  += float(self.current_time[i][selected_vecs[i]]-self.time_windows[i,selected_nodes[i],1])  
-            self.arrived_time[i][selected_vecs[i]] += [self.current_time[i][selected_vecs[i]]]
-            self.travel_cost[i][selected_vecs[i]] += float(travel_time_vecs[i])
-            self.current_location_idx[i][selected_vecs[i]] = selected_nodes[i]
-            # after arriving, and start service, add service time to current time
-            self.current_time[i][selected_vecs[i]] += self.service_durations[i, selected_nodes[i]]
-            #recompute travel time
+        #add demands
+        selected_nodes_demands = self.demands[batch_idx, selected_nodes]
+        self.current_load[batch_idx, selected_vecs] += selected_nodes_demands
+        self.num_visited_nodes[batch_idx, selected_vecs] += 1
+        self.tour_list[batch_idx, selected_vecs, self.num_visited_nodes[batch_idx, selected_vecs]] = selected_nodes
+        self.departure_time_list[batch_idx, selected_vecs, self.num_visited_nodes[batch_idx, selected_vecs]] = self.current_time[batch_idx, selected_vecs]
+        #add to travel time to current time
+        self.current_time[batch_idx, selected_vecs] += travel_time_vecs
+
+        # now filter the actions or the selected vehicles based on their current time
+        # if early, then ceil,
+        # if late, then add to penalty  
+        selected_vecs_current_time = self.current_time[batch_idx, selected_vecs]
+        selected_nodes_tw = self.time_windows[batch_idx, selected_nodes]
+        is_too_early = selected_vecs_current_time <= selected_nodes_tw[:,0]
+        if np.any(is_too_early):
+            self.current_time[batch_idx[is_too_early], selected_vecs[is_too_early]] = selected_nodes_tw[is_too_early,0]
+        is_too_late = selected_vecs_current_time > selected_nodes_tw[:,1]
+        if np.any(is_too_late):
+            self.late_penalty[batch_idx[is_too_late], selected_vecs[is_too_late]] += (selected_vecs_current_time[is_too_late]-selected_nodes_tw[is_too_late,1])
+        
+        self.arrived_time[batch_idx, selected_vecs, self.num_visited_nodes[batch_idx, selected_vecs]] = self.current_time[batch_idx, selected_vecs]
+        self.travel_cost[batch_idx, selected_vecs] += travel_time_vecs 
+        self.current_location_idx[batch_idx, selected_vecs] = selected_nodes
+        # after arriving, and start service, add service time to current time
+        self.current_time[batch_idx, selected_vecs] += self.service_durations[batch_idx, selected_nodes]
         self.travel_time_list = self.get_travel_time()
 
     def get_state(self):
@@ -271,11 +296,14 @@ class BPDPLP_Env(object):
         will it be considered? 
     """
     def finish(self):
+        repeated_vec_idx = np.arange(self.max_num_vehicles)[np.newaxis, :]
+        repeated_vec_idx = np.repeat(repeated_vec_idx, self.batch_size, axis=0).flatten()
         travel_time = self.get_travel_time()
-        for i in range(self.batch_size):
-            for k in range(self.num_vehicles[i]):
-                self.travel_cost[i][k] += travel_time[i][k,0]
+        self.travel_cost[self.batch_vec_idx, repeated_vec_idx] += travel_time[self.batch_vec_idx, repeated_vec_idx, 0]
+        # for i in range(self.batch_size):
+        #     for k in range(self.num_vehicles[i]):
+        #         self.travel_cost[i][k] += travel_time[i][k,0]
         #batch-wise travel cost and penalty
-        travel_cost = np.concatenate([np.asanyarray(self.travel_cost[i]).sum(keepdims=True) for i in range(self.batch_size)])
-        late_penalty = np.concatenate([np.asanyarray(self.late_penalty[i]).sum(keepdims=True) for i in range(self.batch_size)])
+        travel_cost = self.travel_cost.sum(axis=1)
+        late_penalty = self.late_penalty.sum(axis=1)
         return self.tour_list, self.arrived_time, self.departure_time_list, travel_cost, late_penalty
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import pathlib
 import numpy as np
 import torch
 
+from bpdplp.bpdplp_env import BPDPLP_Env
+
 
 def encode(agent, static_features):
     num_requests = int((static_features.shape[1]-1)//2)
@@ -37,41 +39,28 @@ i hope it doesn't affect the agent training,
 i think it will not because we will mask the glimpse computation
 too. i guess.
 """
-def solve_decode_only(agent, env, node_embeddings, fixed_context, glimpse_K_static, glimpse_V_static, logits_K_static):
+def solve_decode_only(agent, env:BPDPLP_Env, node_embeddings, fixed_context, glimpse_K_static, glimpse_V_static, logits_K_static):
     batch_size, num_nodes, embed_dim = node_embeddings.shape
     batch_idx = np.arange(batch_size)
     sum_logprobs = torch.zeros((batch_size,), device=agent.device, dtype=torch.float32)
     sum_entropies = torch.zeros((batch_size,), device=agent.device, dtype=torch.float32)
     static_features, vehicle_dynamic_features, node_dynamic_features, feasibility_mask = env.begin()
+    vehicle_dynamic_features = torch.from_numpy(vehicle_dynamic_features).to(agent.device, dtype=torch.float32)
+    node_dynamic_features = torch.from_numpy(node_dynamic_features).to(agent.device, dtype=torch.float32)
+    feasibility_mask = torch.from_numpy(feasibility_mask).to(agent.device, dtype=bool)
     num_vehicles = env.num_vehicles
     max_num_vehicles = int(np.max(num_vehicles))
     num_vehicles_cum = np.concatenate([np.asanyarray([0]),np.cumsum(num_vehicles)])
     vehicle_batch_idx = np.concatenate([ np.asanyarray([i]*num_vehicles[i]) for i in range(batch_size)])
     vehicle_idx = np.concatenate([np.arange(num_vehicles[i]) for i in range(batch_size)])
-    
     # expanding glimpses
     glimpse_V_static = glimpse_V_static.unsqueeze(2).expand(-1,-1,max_num_vehicles,-1,-1)
     glimpse_K_static = glimpse_K_static.unsqueeze(2).expand(-1,-1,max_num_vehicles,-1,-1)
     logits_K_static = logits_K_static.unsqueeze(1).expand(-1,max_num_vehicles,-1,-1)
     fixed_context = fixed_context.unsqueeze(1).expand(-1,max_num_vehicles,-1)
-
-    # padding feasibility mask and concatenating, ready for torch
-    num_vehicle_diffs = max_num_vehicles-num_vehicles
-    feasibility_mask = [np.pad(feasibility_mask[i],((0,num_vehicle_diffs[i]),(0,0)))[np.newaxis,:,:] for i in range(batch_size)]
-    feasibility_mask = np.concatenate(feasibility_mask,axis=0)
-    feasibility_mask = torch.from_numpy(feasibility_mask).to(agent.device)
-    
-    vehicle_dynamic_features = [np.pad(vehicle_dynamic_features[i], ((0,num_vehicle_diffs[i]),(0,0)))[np.newaxis,:,:] for i in range(batch_size)]
-    vehicle_dynamic_features = np.concatenate(vehicle_dynamic_features, axis=0)
-    vehicle_dynamic_features = torch.from_numpy(vehicle_dynamic_features).to(agent.device)
-
-    node_dynamic_features = [np.pad(node_dynamic_features[i], ((0,num_vehicle_diffs[i]),(0,0),(0,0)))[np.newaxis,:,:] for i in range(batch_size)]
-    node_dynamic_features = np.concatenate(node_dynamic_features, axis=0)
-    node_dynamic_features = torch.from_numpy(node_dynamic_features).to(agent.device)
     while torch.any(feasibility_mask):
-        prev_node_embeddings = torch.zeros((batch_size, max_num_vehicles, embed_dim), dtype=torch.float32, device=agent.device)
-        current_location_idx = np.concatenate([env.current_location_idx[i] for i in range(batch_size)])
-        prev_node_embeddings[vehicle_batch_idx, vehicle_idx, :] = node_embeddings[vehicle_batch_idx, current_location_idx, :]
+        prev_node_embeddings = node_embeddings[env.batch_vec_idx, env.current_location_idx.flatten(), :]
+        prev_node_embeddings = prev_node_embeddings.view((batch_size,max_num_vehicles,-1))
         forward_results = agent.forward(node_embeddings,
                                         fixed_context,
                                         prev_node_embeddings,
@@ -89,18 +78,10 @@ def solve_decode_only(agent, env, node_embeddings, fixed_context, glimpse_K_stat
         sum_logprobs += logprob_list
         sum_entropies += entropy_list
         vehicle_dynamic_features, node_dynamic_features, feasibility_mask = env.get_state()
-        feasibility_mask = [np.pad(feasibility_mask[i],((0,num_vehicle_diffs[i]),(0,0)))[np.newaxis,:,:] for i in range(batch_size)]
-        feasibility_mask = np.concatenate(feasibility_mask,axis=0)
-        feasibility_mask = torch.from_numpy(feasibility_mask).to(agent.device)
+        vehicle_dynamic_features = torch.from_numpy(vehicle_dynamic_features).to(agent.device, dtype=torch.float32)
+        node_dynamic_features = torch.from_numpy(node_dynamic_features).to(agent.device, dtype=torch.float32)
+        feasibility_mask = torch.from_numpy(feasibility_mask).to(agent.device, dtype=bool)
         
-        vehicle_dynamic_features = [np.pad(vehicle_dynamic_features[i], ((0,num_vehicle_diffs[i]),(0,0)))[np.newaxis,:,:] for i in range(batch_size)]
-        vehicle_dynamic_features = np.concatenate(vehicle_dynamic_features, axis=0)
-        vehicle_dynamic_features = torch.from_numpy(vehicle_dynamic_features).to(agent.device)
-
-        node_dynamic_features = [np.pad(node_dynamic_features[i], ((0,num_vehicle_diffs[i]),(0,0),(0,0)))[np.newaxis,:,:] for i in range(batch_size)]
-        node_dynamic_features = np.concatenate(node_dynamic_features, axis=0)
-        node_dynamic_features = torch.from_numpy(node_dynamic_features).to(agent.device)
-
     tour_list, arrived_time_list, departure_time_list, travel_costs, late_penalties = env.finish()
     return tour_list, arrived_time_list, departure_time_list, travel_costs, late_penalties, sum_logprobs, sum_entropies
 
