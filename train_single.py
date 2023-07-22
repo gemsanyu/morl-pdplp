72b7e69fd5aa55ec723c7da2621fa8857c157aed
gemilang <git.gemilang.santiyuda@gmail.com>
2023-07-22 22:15:39 +0700

    coba biar bisa versi compile

--- train_single.py
+++ train_single.py
@@ -14,9 +14,11 @@ from bpdplp.bpdplp_dataset import BPDPLP_Dataset
 from utils import encode, solve_decode_only, update
 from utils import save, prepare_args
 from setup import setup
+
         
-def train_one_epoch(args, agent, opt, train_dataset, tb_writer, epoch):
+def train_one_epoch(args, agent, best_agent, opt, train_dataset, tb_writer, epoch):
     agent.train()
+    best_agent.eval()
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2, pin_memory=True)
     sum_advantage = 0
     sum_training_travel_costs = 0
@@ -33,10 +35,21 @@ def train_one_epoch(args, agent, opt, train_dataset, tb_writer, epoch):
         solve_results = solve_decode_only(agent, env, node_embeddings, fixed_context, glimpse_K_static, glimpse_V_static, logits_K_static)
         
         tour_list, arrived_time_list, departure_time_list, travel_costs, late_penalties, reward_list, logprob_list, sum_entropies = solve_results
-        advantage_list = (reward_list - reward_list.mean(axis=1, keepdims=True))/reward_list.std(axis=1, keepdims=True)
-        advantage_list = np.sum(advantage_list, axis=-1)
+        
+        # computing critic
+        with torch.no_grad():
+            encode_results = encode(best_agent, static_features)
+            node_embeddings, fixed_context, glimpse_K_static, glimpse_V_static, logits_K_static = encode_results
+            greedy_solve_results = solve_decode_only(best_agent, env, node_embeddings, fixed_context, glimpse_K_static, glimpse_V_static, logits_K_static)    
+            _, _, _, greedy_travel_costs, greedy_late_penalties, _, _, _ = greedy_solve_results
+
+        score = travel_costs + late_penalties
+        greedy_score = greedy_travel_costs + greedy_late_penalties
+        advantage_list = score-greedy_score
+        advantage_list = (advantage_list - advantage_list.mean())/advantage_list.std()
+        logprob_list = logprob_list.sum(dim=-1)
         loss = logprob_list*torch.from_numpy(advantage_list).to(agent.device)
-        loss = loss.sum(dim=-1).mean() - 0.05*sum_entropies.mean()
+        loss = loss.mean() - 0.05*sum_entropies.mean()
         update(agent, opt, loss, args.max_grad_norm)
         
         sum_advantage += advantage_list.sum()
@@ -51,7 +64,7 @@ def train_one_epoch(args, agent, opt, train_dataset, tb_writer, epoch):
     tb_writer.add_scalar("Training Entropies", sum_training_entropies/args.num_training_samples, epoch)
         
 @torch.no_grad()
-def validate_one_epoch(agent, validation_dataset, best_validation_score, best_agent_state_dict, tb_writer, epoch):
+def validate_one_epoch(agent, validation_dataset, best_validation_score, best_agent, tb_writer, epoch):
     agent.eval()
     validation_dataloader = DataLoader(validation_dataset, batch_size=args.batch_size, num_workers=2, pin_memory=True)
     validation_late_penalty_list = []
@@ -89,24 +102,28 @@ def validate_one_epoch(agent, validation_dataset, best_validation_score, best_ag
 
     if best_validation_score is None:
         best_validation_score = validation_score_list
-        best_agent_state_dict = copy.deepcopy(agent.state_dict())
-        return best_validation_score, best_agent_state_dict
+        best_agent = copy.deepcopy(agent)
+        return best_validation_score, best_agent
     ret = wilcoxon(validation_score_list, best_validation_score, alternative='less')
     tb_writer.add_scalar("pvalue", ret.pvalue, epoch)
     if ret.pvalue < 0.05:
         best_validation_score = validation_score_list
-        best_agent_state_dict = copy.deepcopy(agent.state_dict())
-    return best_validation_score, best_agent_state_dict
+        best_agent = copy.deepcopy(agent)
+    return best_validation_score, best_agent
 
 def run(args):
     agent, opt, best_agent_state_dict, best_validation_score, tb_writer, last_epoch = setup(args)
-    validation_dataset = BPDPLP_Dataset(num_samples=args.num_validation_samples, mode="validation", num_vehicles_list=[1,3,5], num_clusters_list=[3,5,8])
+    best_agent = copy.deepcopy(agent)
+    if best_agent_state_dict is not None:
+        best_agent.load_state_dict(best_agent_state_dict)
+    validation_dataset = BPDPLP_Dataset(num_samples=args.num_validation_samples, mode="validation")
     train_dataset = BPDPLP_Dataset(num_samples=args.num_training_samples, mode="training")
-    
+    comp_agent = torch.compile(agent)
+    comp_best_agent = torch.compile(best_agent)
     for epoch in range(last_epoch+1, args.max_epoch):
-        train_one_epoch(args, agent, opt, train_dataset, tb_writer, epoch)
-        best_validation_score, best_agent_state_dict = validate_one_epoch(agent, validation_dataset, best_validation_score, best_agent_state_dict, tb_writer, epoch)
-        save(agent, opt, best_validation_score, best_agent_state_dict, epoch, args.title)
+        train_one_epoch(args, comp_agent, comp_best_agent, opt, train_dataset, tb_writer, epoch)
+        best_validation_score, best_agent = validate_one_epoch(comp_agent, validation_dataset, best_validation_score, comp_best_agent, tb_writer, epoch)
+        save(agent, opt, best_validation_score, best_agent.state_dict(), epoch, args.title)
 
 if __name__ == "__main__":
     args = prepare_args()
