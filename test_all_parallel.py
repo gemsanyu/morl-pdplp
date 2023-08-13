9b0185c3c257f8389c47c6f69d14e412d9e48f4d
gemilanghpc <git.gemilang.santiyuda@gmail.com>
2023-08-13 00:25:43 +0000

    tested again

--- test_all_parallel.py
+++ test_all_parallel.py
@@ -24,7 +24,7 @@ def test_all_parallel(instances_name_list, num_vehicles_list, title, num_ray):
     #     test(*config)
 
 if __name__ == "__main__":
-    title="hnc-phn"
+    title="hnc-phn-po-init"
     num_ray=200
 
     instances_name_list = [
