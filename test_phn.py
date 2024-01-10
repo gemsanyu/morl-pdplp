8966dac09c40c9bd010cdf06e37ba70c099fd0b7
gemilanghpc <git.gemilang.santiyuda@gmail.com>
2024-01-10 01:32:12 +0000

    tested 1000

--- test_phn.py
+++ test_phn.py
@@ -86,7 +86,6 @@ def test(agent, phn, test_batch, x_file, y_file, t_file, num_ray=200):
 
 def run(args):
     agent, _, phn, _, _, _, _, _, test_batch, _ = setup_phn(args, validation=True)
-    exit()
     result_dir = pathlib.Path(".")/"result"/args.title
     result_dir.mkdir(parents=True, exist_ok=True)
     title = args.test_instance_name +"-"+str(args.test_num_vehicles) + "-" + args.title
